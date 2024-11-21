543240b8989b268d23bb9ccb7a5ad3d50fdbcc22
cheysea <184555093+cheysea@users.noreply.github.com>
2024-11-20 19:20:44 -0600

    restructured initial files and added basic classes

---
+++ web_tools/create_session.py
@@ -0,0 +1,7 @@
+class CreateSession(): session
+    def __init__():
+        session: Session = requests.Session()
+        
+        return session
+        
+    
---
+++ web_tools/scraping/tests/authenticator_tests.py
@@ -0,0 +1,3 @@
+from chelseas_cookbook.web_tools.scraping.authenticator import Authenticator
+
+test_authenticator = Authenticator()
