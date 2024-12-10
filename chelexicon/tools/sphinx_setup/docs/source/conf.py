d4a0a9cac664524ddcc8df4567ef34f57bc1f112
cheysea <chelseascarlile@gmail.com>
2024-12-10 15:41:53 -0600

    Completed the Conf class and documented it.

--- chelexicon/tools/sphinx_setup/docs/source/conf.py
+++ chelexicon/tools/sphinx_setup/docs/source/conf.py
@@ -22,38 +22,94 @@ class Conf():
     """
     def __init__(self,
                  is_testing: bool =False,
+                 project: str = 'Project Name',
+                 copyright: str = 'Year, Author',
+                 author: str = 'Author',
+                 release: str = '1.0',
+                 extensions: list =  None,
+                 autosummary_generate: bool = True,
+                 templates_path: list = None,
+                 exclude_patterns: list = None,
+                 available_html_themes: list = None,
+                 html_theme: str = None,
+                 html_static_path: list = None,
                  ) -> None:
-        """
-        Represents a configuration entity with project details and settings.
+        """Represents a configuration entity with project details and settings."""
 
-        :attributes:
-           :is_testing: A boolean flag indicating if the instance is in testing mode.
-           :project: The name of the project.
-           :copyright: The copyright information including year and author.
-           :author: The author of the project.
-           :release: The release version of the project.
-        """
+        # Set the variable for testing.
         self.is_testing: bool = is_testing
 
-        self.project = 'Project Name'
-        self.copyright = 'Year, Author'
-        self.author = 'Author'
-        self.release = '1.0'
+        # Configure project settings.
+        self.project = project
+        self.copyright = copyright
+        self.author = author
+        self.release = release
 
-    def configure_sphinx(self):
+        # Configure extensions and autosummary.
+        if extensions is None:
+            self.extensions =  [
+                'sphinx.ext.autodoc',
+                'sphinx.ext.autosummary',
+                'sphinx_rtd_theme',
+                'sphinx_wagtail_theme'
+            ]
+        self.autosummary_generate = autosummary_generate
+
+        if templates_path is None:
+            self.templates_path = ['_templates']
+        if exclude_patterns is None:
+            self.exclude_patterns = []
+
+        # Configure the html_theme for Sphinx based on user preference
+        # from most preferred to least preferred.
+        if available_html_themes is None:
+            self.available_html_themes = [
+                #'sphinx_documatt_theme',
+                'sphinx_wagtail_theme',
+                'sphinx_rtd_theme',
+                'alabaster',]
+        self.builtin_themes = [
+            'alabaster',
+            'classic'
+            'sphinxdoc',
+            'scrolls',
+            'agogo',
+            'traditional',
+            'nature',
+            'haiku',
+            'pyramid',
+            'bizstyle',
+
+        ]
+        if html_theme is None:
+            self.html_theme = self.generate_html_theme()
+        else:
+            self.html_theme = html_theme
+
+        # Set the path for the static folder
+        if html_static_path is None:
+            self.html_static_path = ['_static']
+        else:
+            self.html_static_path = html_static_path
+
+    def configure_sphinx(self) -> dict:
         """
         A method to collect all the configuration settings for Sphinx.
-        :explanation: This will keep them better organized and easier to adjust
-        since we have put them all in a class.
+
+        :returns: A dictionary of configuration settings. With the key:value
+        being the setting name and the value of the setting.
         """
         self.register_project_path()
-        print(f"Current Directory Debug: {os.getcwd()}")
-        print(f"Sphinx Version: {sphinx.__version__}")
+
+        config_item: dict = { key: value for key,value in self.__dict__.items() if not key.startswith('__') }
+        return config_item
 
     def register_project_path(self) -> None:
         """
         Register the project path to the Python path.
-        With a code structure that looks like:::
+
+        With a code structure that looks like:
+        ::
            project_root/
               |- project_directory
                  |- __init__.py
@@ -71,42 +127,50 @@ class Conf():
 
         For testing and for this template package itself we go back three
         directories (../../../) so we can call on the project_root as the top level
-        package;
-
+        package
         """
         if self.is_testing:
             sys.path.insert(0, os.path.abspath('../../..'))
         else:
             sys.path.insert(0, os.path.abspath('../..'))
 
+    def generate_html_theme(self):
+        """ Reads the available html_themes and returns the first one that is available."""
+        for html_theme in self.available_html_themes:
+            if html_theme in sys.modules or self.is_testing:
+                available_theme =  html_theme
+                break
+            elif html_theme == self.html_themes[-1]:
+                available_theme = 'alabaster'
+        return available_theme
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 conf = Conf(is_testing=True)
-conf.configure_sphinx()
+config_item = conf.configure_sphinx()
 
-project = 'Project Name'
-copyright = 'Year, Author'
-author = 'Author'
-release = '1.0'
+project = config_item['project']
+copyright = config_item['copyright']
+author = config_item['author']
+release = config_item['release']
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx_rtd_theme',
-]
-autosummary_generate = True
+extensions = config_item['extensions']
+autosummary_generate = config_item['autosummary_generate']
 
-templates_path = ['_templates']
-exclude_patterns = []
+templates_path = config_item['templates_path']
+exclude_patterns = config_item['exclude_patterns']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = config_item['html_theme']
+html_static_path = config_item['html_static_path']
+
+
