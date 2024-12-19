61cdcb4376626afee53135c94c01602cddaf8a50
cheysea <chelseascarlile@gmail.com>
2024-12-19 14:47:56 -0600

    Add files via upload

---
+++ chelexicon/logging_config/config/configure_flask.py
@@ -0,0 +1,10 @@
+import logging.config
+log = logging.getLogger(__name__)
+
+
+class ConfigureFlask():
+    TESTING = True
+
+    def configure_flask(self, app):
+        app.config.from_object("chelexicon.logging.config.config")
+
